c792989900184c7894ace27f51b20b450850ae8b
Laura <laura.reivinen@gmail.com>
2019-02-07 09:59:44 +0200

    fixing internal server error

--- application/auth/models.py
+++ application/auth/models.py
@@ -31,7 +31,7 @@ class User(Base):
         return True
 
     @staticmethod
-    def find_users_with_no_tasks(done=0):
+    def find_users_with_no_tasks(done=False):
         stmt = text("SELECT Account.id, Account.name FROM Account"
                      " LEFT JOIN Task ON Task.account_id = Account.id"
                      " WHERE (Task.done IS null OR Task.done = :done)"
